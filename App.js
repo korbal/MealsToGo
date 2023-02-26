1ddb14afd1a167945b797dc8f9ebf1618867cec2
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-26 11:06:22 +0100

    yolo

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Succesfully inited, yay!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Hi</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
