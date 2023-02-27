336e76ff706ca0f32e644faaaaa51af963a728af
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-27 14:06:53 +0100

    searchbar added with react native paper

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Searchbar } from "react-native-paper";
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 console.log(StatusBar.currentHeight);
@@ -8,10 +9,10 @@ export default function App() {
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>search</Text>
+          <Searchbar />
         </View>
         <View style={styles.list}>
-          <Text>list</Text>
+          <Text>list dfdf</Text>
         </View>
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
@@ -26,7 +27,7 @@ const styles = StyleSheet.create({
   },
   search: {
     padding: 16,
-    backgroundColor: "green",
+    // backgroundColor: "",
   },
   list: {
     flex: 1,
