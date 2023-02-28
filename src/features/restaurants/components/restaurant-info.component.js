a512782af3035255d16c35f8a91a1b2e6438cafe
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-28 08:26:01 +0100

    restaurant card added

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
@@ -13,5 +14,16 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
-  return <Text> {name}</Text>;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover style={styles.cover} key={name} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: { backgroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
