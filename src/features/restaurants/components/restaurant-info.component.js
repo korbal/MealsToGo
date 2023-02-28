08615b6fae995b645675712de23f6d16fc42cce6
Balint Korosi <balint.j.korosi@gmail.com>
2023-02-28 08:05:17 +0100

    yolo

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,4 +1,17 @@
 import React from "react";
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => <Text>Restaurant info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/07/steak-recipe-featured-image.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text> {name}</Text>;
+};
